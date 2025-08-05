64a19d0dcb6d34885b617ea99b2077c8784d320b
sadan <117494111+sadan4@users.noreply.github.com>
2025-08-04 23:36:07 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A basic flake with a shell";
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-25.05";
+    };
+    systems = {
+      url = "github:nix-systems/default";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            hello
+          ];
+          LD_LIBRARY_PATH = pkgs.lib.strings.makeLibraryPath (
+            with pkgs;
+            [
+
+            ]
+          );
+          shellHook = ''
+            #shell config here
+          '';
+        };
+      }
+    );
+}
